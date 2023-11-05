7106d1920249368b1e74a6a549be451d09debecc
khalid harbi <ka901od@gmail.com>
2023-11-06 02:29:49 +0300

    add line

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
+
+$conn = mysqli_connect('localhost', 'root', 'root', 'win');
+
+if(!$conn){
+    echo 'Eroor: '. mysqli_connect_error();
+}
+
+
 $firstName =  $_POST['firstName'];
 $lastName =   $_POST['lastName'];
 $email   =    $_POST['email'];
 
-echo $firstName . ' ' . $lastName . ' ' . $email;
+if (isset($_POST['submit'])){
+    echo $firstName . ' ' . $lastName . ' ' . $email;
+}
 
 ?>
 
