c98a1a87cbb8b8f7c9854472c66daac632c37b0a
khalid harbi <ka901od@gmail.com>
2023-11-05 22:08:30 +0300

    form

--- index.php
+++ index.php
@@ -9,6 +9,13 @@
     <title>Document</title>
 </head>
 <body>
+
+  <form action="index.php">
+    <input type="text" name="firstName" id="firstName" placeholder="first name">
+    <input type="text" name="lastName" id="lastName" placeholder="last name">
+    <input type="text" name="email" id="email" placeholder="email">
+    <input type="submit" name="submit" value="send">
+  </form>
     
 
     <script src="./js/script.js"></script>
