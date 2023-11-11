ba30b94c11bd290dbf2665396028f55c64bb76a3
khalid harbi <ka901od@gmail.com>
2023-11-11 22:35:20 +0300

    commit

--- inc/form.php
+++ inc/form.php
@@ -1,22 +1,32 @@
 <?php
+
 $firstName =  $_POST['firstName'];
 $lastName =   $_POST['lastName'];
 $email   =    $_POST['email'];
 
+$errors = [
+    'firstNameError' => '',
+    'lastNameError' => '',
+    'emailError' => '',
+];
+
 if (isset($_POST['submit'])){
-    //echo $firstName . ' ' . $lastName . ' ' . $email;
+
+    $firstName =  mysqli_real_escape_string($conn, $_POST['firstName']);
+    $lastName =   mysqli_real_escape_string($conn, $_POST['lastName']);
+    $email   =    mysqli_real_escape_string($conn, $_POST['email']);
 
     $sql = "INSERT INTO users(firstName, lastName, email) 
             VALUES ('$firstName', '$lastName', '$email')";
 
     if(empty($firstName)){
-        echo 'Enter first name';
+        $errors['firstNameError'] = 'يرجى ادخال الاسم الاول';
     }elseif(empty($lastName)){
-        echo 'Enter last name';
+        $errors['lastNameError'] = 'يرجى ادخال الاسم الاخير';
     }elseif(empty($email)){
-        echo 'Enter email';
+        $errors['emailError'] = 'يرجى ادخال الايميل';
     }elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-      echo 'Enter a right email';
+        $errors['emailError'] = 'يرجى ادخال ايميل صحيح';
     }else{
       if(mysqli_query($conn, $sql)){
         header('Location: index.php');
--- index.php
+++ index.php
@@ -6,9 +6,7 @@ $sql = 'SELECT * FROM users';
 $result = mysqli_query($conn, $sql);
 $users = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-echo '<pre>';
-print_r($users);
-echo '</pre>';
+
 
 mysqli_free_result($result);
 mysqli_close($conn);
@@ -16,23 +14,73 @@ mysqli_close($conn);
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" dir="rtl">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/bootstrap.rtl.min.css" >
     <link rel="stylesheet" href="./css/style.css">
     <title>Document</title>
 </head>
+
 <body>
 
-  <form action="index.php" method="POST">
-    <input type="text" name="firstName" id="firstName" placeholder="first name">
-    <input type="text" name="lastName" id="lastName" placeholder="last name">
-    <input type="text" name="email" id="email" placeholder="email">
-    <input type="submit" name="submit" value="send">
-  </form>
-    
+<div class="container">
+<!--------
+<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+    <div class="col-md-5 p-lg-5 mx-auto my-5">
+      <h1 class="display-4 fw-normal">اربح مع طيبة</h1>
+      <p class="lead fw-normal">باقي على فتح التسجيل</p>
+      <p class="lead fw-normal">للسحب على نسخة مجانية من برنامج</p>
+      <a class="btn btn-outline-secondary" href="#">Coming soon</a>
+    </div>
+    <div class="product-device shadow-sm d-none d-md-block"></div>
+    <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+  </div>
 
+
+  <ul class="list-group list-group-flush">
+  <li class="list-group-item">تابع البث المباشر على صفحتي على فيسبوك بالتاريخ المذكور اعلاه</li>
+  <li class="list-group-item">سأقوم ببث مباشر لمدة ساعة عبارة عن أسئلة و أجوبة حرة للجميع</li>
+  <li class="list-group-item">خلال فترة الساعة سيتم فتح صفحة التسجيل هنا حيث ستقوم بتسجيل اسمك و أيميلك</li>
+  <li class="list-group-item">بنهاية البث سيتم اختيار اسم واحد من قاعدة البيانات بشكل عشوائي</li>
+  <li class="list-group-item">الرابح سيحصل على نسخة أصلية من برنامج كامتازيا</li>
+</ul>
+-----> 
+
+<form class="mt-5" action="index.php" method="POST">
+  <h3>الرجاء أدخل معلوماتك</h3>
+
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">الاسم الأول</label>
+    <input type="text" name="firstName" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text error"><?php echo $errors['firstNameError'] ?></div>
+  </div>
+
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">الاسم الأخير</label>
+    <input type="text" name="lastName" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text error"><?php echo $errors['lastNameError'] ?></div>
+  </div>
+
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">البريد الألكتروني</label>
+    <input type="text" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text error"><?php echo $errors['emailError'] ?></div>
+  </div>
+
+
+  <button type="submit" class="btn btn-primary">ارسال المعلومات</button>
+</form>
+
+<!-----
+  <?php foreach($users as $user): ?>
+   <h1><?php echo htmlspecialchars($user['firstName']) . ' ' . htmlspecialchars($user['lastName']) . '<br>' . htmlspecialchars($user['email']); ?></h1>
+  <?php endforeach; ?>
+---->   
+
+  <script src="./js/bootstrap.bundle.min.js"></script>
     <script src="./js/script.js"></script>
 </body>
 </html>
